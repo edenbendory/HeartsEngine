f14b30cfa7019bdc0842fe024ac1aa016f129045
Eden Bendory <eb1889@princeton.edu>
2025-03-25 11:19:11 -0400

    runtrials

--- runtrials.sh
+++ runtrials.sh
@@ -22,8 +22,8 @@ awk '
 /UCTPlayer_RandomPlayer:/     { uct_random_sum += $2; uct_random_count++ }
 /MCTSPlayer:/     { mcts_sum += $2; mcts_count++ }
 /UCTPlayer_MCTSPlayer:/     { uct_mcts_sum += $2; uct_mcts_count++ }
-/LookAheadPlayer:/     { lookahead_sum += $2; lookahead_count++ }
-/UCTPlayer_LookAheadPlayer:/     { uct_lookahead_sum += $2; uct_lookahead_count++ }
+# /LookAheadPlayer:/     { lookahead_sum += $2; lookahead_count++ }
+# /UCTPlayer_LookAheadPlayer:/     { uct_lookahead_sum += $2; uct_lookahead_count++ }
 END {
     printf "HighLowPlayer Average: %.2f\n", highlow_sum / highlow_count;
     printf "UCTPlayer Average: %.2f\n", uct_highlow_sum / uct_highlow_count;
@@ -37,8 +37,8 @@ END {
     printf "UCTPlayer Average: %.2f\n", uct_mcts_sum / uct_mcts_count;
     printf"\n";
 
-    printf "LookAheadPlayer Average: %.2f\n", lookahead_sum / lookahead_count;
-    printf "UCTPlayer Average: %.2f\n", uct_lookahead_sum / uct_lookahead_count;
-    printf"\n";
+    # printf "LookAheadPlayer Average: %.2f\n", lookahead_sum / lookahead_count;
+    # printf "UCTPlayer Average: %.2f\n", uct_lookahead_sum / uct_lookahead_count;
+    # printf"\n";
 }
 ' "$output_file"
